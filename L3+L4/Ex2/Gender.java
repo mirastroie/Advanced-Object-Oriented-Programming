04da15df8d5586da401613b1a97839427cbb462a
Mira Stroie <miramaria27@yahoo.com>
2021-12-14 20:38:23 +0200

    Delete L3+L4 directory

--- L3+L4/Ex2/Gender.java
+++
@@ -1,7 +0,0 @@
-package L3.Ex2;
-
-//An enum is a special "class" that represents a group of
-// constants (unchangeable variables, like final variables).
-public enum Gender {
-    M, F, Other
-}
--- L3+L4/Ex4/model/Quadrilateral.java
+++
@@ -1,5 +0,0 @@
-package L3.Ex4.model;
-
-public abstract class Quadrilateral {
-    public abstract double Area();
-}
